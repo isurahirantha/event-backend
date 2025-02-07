c3797525784b7841d66862fab72bd9a87d070b3d
Isura Hirantha <isura_s@epiclanka.net>
2025-02-07 14:31:22 +0530

    1. user role included to auth response. 2. Refactored

--- src/main/java/com/codeloon/ems/service/InventoryServiceImpl.java
+++ src/main/java/com/codeloon/ems/service/InventoryServiceImpl.java
@@ -1,17 +1,11 @@
 package com.codeloon.ems.service;
 
 import com.codeloon.ems.dto.InventoryDto;
-import com.codeloon.ems.dto.UserDto;
 import com.codeloon.ems.entity.Inventory;
-import com.codeloon.ems.entity.Role;
-import com.codeloon.ems.entity.User;
-import com.codeloon.ems.entity.UserPersonalData;
 import com.codeloon.ems.model.DataTableBean;
 import com.codeloon.ems.repository.InventoryRepository;
-import com.codeloon.ems.util.DataVarList;
 import com.codeloon.ems.util.ResponseBean;
 import com.codeloon.ems.util.ResponseCode;
-import com.codeloon.ems.util.UserUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -20,15 +14,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
